1cc184bf3af5430f67ee8eadae8463f832912c0c
Marvin M <39344769+M123-dev@users.noreply.github.com>
2026-02-18 21:43:26 +0100

    Adjust value assignment logic in event.py (#311)
    Modified value assignment to allow None when amount is zero.

--- pytr/event.py
+++ pytr/event.py
@@ -538,9 +538,7 @@ class Event:
             order_dict,
         ) = (None,) * 15
 
-        value: Optional[float] = (
-            v if (v := event_dict.get("amount", {}).get("value", None)) is not None and v != 0.0 else None
-        )
+        value: Optional[float] = v if (v := event_dict.get("amount", {}).get("value", None)) is not None else None
 
         title = event_dict["title"]
         subtitle = event_dict["subtitle"]
